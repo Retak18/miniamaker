35456ee9e1bd187e8fe7a764ccc87a7b379e8fee
Tarek <tarek_lamarti@live.fr>
2025-02-04 15:03:40 +0100

    fusion style

---
+++ src/Controller/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController{
+    #[Route('/page', name: 'app_page', methods: ['GET'])]
+    public function index(): Response
+    {
+        if(!$this->getUser())
+        {
+            return $this->render('page/lp.html.twig');
+        }
+        return $this->render('page/homepage.html.twig');
+    }
+}
