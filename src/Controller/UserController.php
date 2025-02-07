9c58ea83f345ebf431b3d3ffde62c0aa7c072ef2
Tarek <tarek_lamarti@live.fr>
2025-02-07 14:35:23 +0100

    mise en forme

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,45 +2,54 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class UserController extends AbstractController{
+final class UserController extends AbstractController
+{
     #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil à été mis à jour');
+            return $this->redirectToRoute('app_profile');
+        }
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'userForm' => $form,
         ]);
     }
 
     #[Route('/complete', name: 'app_complete', methods: ['POST'])]
     public function complete(Request $request, EntityManagerInterface $em): Response
     {
-        $data = $request->getPayload();
-        // $username = $request->getPayload('username');
-        // $fullname = $request->getPayload('fullname');
-        if(!empty($data->get('username')) && !empty($data->get('fullname'))) {
-           
-            dd('test');
-            //enregistrer les données dans la base de données
-            $user = $this->getUser(); //ici on récupèr l'utilisateur actuel
+        $data = $request->getPayload(); // on récupère les données du formulaire
+        if (!empty($data->get('username')) && !empty($data->get('fullname'))) {
+            // Enregistrer les données dans la base de données
+            $user = $this->getUser(); // Ici on récupère l'utilisateur actuel
             $user
-                ->setUsername($data->get('username'))// on met à jour
-                ->setFullname($data->get('fullname'));// on met à jour
+                ->setUsername($data->get('username')) // on met à jour username
+                ->setFullname($data->get('fullname')) // on met à jour fullname
+                ;
+            $em->persist($user); // on persiste l'utilisateur
+            $em->flush(); // on sauvegarde les modifications en base de données
             
-            $em->persist($user); //on persist l'utilisateur
-            $em->flush(); //on sauvegarde les modifications en base de données
-
-        //redirection avec flash message
-        $this->addFlash('success', 'Votre profil est maintenant complet.');
-    } else {
-        $this->addFlash('error', 'Veuillez remplir tous les champs.');
-    }
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil est complété');
+        } else {
+            $this->addFlash('error', 'Vous devez remplir tous les champs');
+        }
         return $this->redirectToRoute('app_profile');
-        
     }
 }
